29d8113596ca37f83e612dafeef90a870129aa30
Leandro <fahurleandro@gmail.com>
2021-05-13 15:18:23 -0700

    python refresher

---
+++ sandbox/arrays.py
@@ -0,0 +1,12 @@
+# declaring an array:
+fruits = ["apple", "banana", "watermelon"]
+
+# loop over an array:
+for fruit in fruits:
+    print(fruit)
+
+# print the ith element of an array:
+print(fruits[0])
+
+# print the size of an array:
+print(len(fruits))
---
+++ sandbox/loops.py
@@ -0,0 +1,13 @@
+# for loops:
+for number in range(0,10):    
+    if(number == 7):
+        print('I am tired now...')
+        break
+    else:
+        print('I am alive!!!')
+
+# while loops:
+i = 0
+while i < 10:
+    print(i)
+    i+=1
